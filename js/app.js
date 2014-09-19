d8b52599e6276e2fdc33039739e6e127689e8899
Mihai Panait <panait_mihai@yahoo.com>
2014-09-19 13:26:26 +0300

    changed picture 5. modified angular service

--- js/app.js
+++ js/app.js
@@ -11,20 +11,20 @@ app.factory('FeauresService', function() {
 	var viewStates = new Array(12);
 	viewStates[0] = ["big", "small", "small",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "big", "small", "small"];
 	viewStates[1] = ["medium", "big", "none",
 					 "medium", "big",
 					 "small", "small", "big"];
-	viewStates[2] = ["small", "small", "big",
+	viewStates[2] = ["medium", "none", "big",
 					 "medium", "big",
-					 "big", "small", "small"];
+					 "small", "small", "big"];
 	viewStates[3] = ["big", "small", "small",
 					 "big", "medium",
 					 "small", "small", "big"];
 	viewStates[4] = ["small", "small", "big",
 					 "medium", "big",
-					 "big", "small", "small"];
-	viewStates[5] = ["big", "small", "small",
+					 "small", "small", "big"];
+	viewStates[5] = ["big", "medium", "none",
 					 "medium", "big",
 					 "big", "small", "small"];
 	viewStates[6] = ["big", "small", "small",
@@ -65,12 +65,31 @@ app.controller('FeaturesController', ['$scope','FeauresService' , function($scop
   	this.isActive = new Array(12);
   	this.isSelected = new Array(12);  	
   	this.allStates = FeauresService.getViewStates();
-  	this.currentState = this.allStates[0]; //default state
+  	this.currentState = this.allStates[0].slice(); //default state
 
   	this.ToggleSelect = function(index){  		
   		this.isSelected[index] = !this.isSelected[index]; 
-  		this.currentState =  this.allStates[index];	
   		this.deselectOthers(index);	
+
+  		if(index === 0 || index === 1 || index === 2){
+
+  			this.currentState[0] =  this.allStates[index][0];
+  			this.currentState[1] =  this.allStates[index][1];
+  			this.currentState[2] =  this.allStates[index][2];	
+
+  		} else if (index === 3 || index === 4 ){
+
+  			this.currentState[3] =  this.allStates[index][3];
+  			this.currentState[4] =  this.allStates[index][4];	
+
+  		} else if (index === 5 || index === 6 || index === 7){
+
+  			this.currentState[5] =  this.allStates[index][5];
+  			this.currentState[6] =  this.allStates[index][6];
+  			this.currentState[7] =  this.allStates[index][7];	
+
+  		}  		
+  		
   	};
 
   	this.deselectAll = function(){
