2ee1e63d3b25377b1a31e47f249c4b38ac3b07a4
Mihai Panait <mihai.panait@outlook.com>
2014-09-29 04:46:30 +0300

    modified top banner

--- js/app.js
+++ js/app.js
@@ -11,85 +11,103 @@ app.factory('FeauresService', function() {
 	var viewStates = new Array(12);
 	viewStates[0] = ["big", "small", "small",
 					 "medium", "big",
-					 "big", "small", "small"];
+					 "big", "small", "small",
+           "medium", "big"];
 	viewStates[1] = ["medium", "big", "none",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[2] = ["medium", "none", "big",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[3] = ["big", "small", "small",
 					 "big", "medium",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[4] = ["small", "small", "big",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[5] = ["big", "medium", "none",
 					 "medium", "big",
-					 "big", "small", "small"];
+					 "big", "small", "small",
+           "medium", "big"];
 	viewStates[6] = ["big", "small", "small",
 					 "medium", "big",
-					 "medium", "big", "none"];
+					 "medium", "big", "none",
+           "medium", "big"];
 	viewStates[7] = ["big", "small", "small",
 					 "medium", "big",
-					 "medium", "none", "big"];
+					 "medium", "none", "big",
+           "medium", "big"];
 	viewStates[8] = ["big", "small", "small",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "big", "medium"];
 	viewStates[9] = ["big", "small", "small",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[10] = ["big", "small", "small",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[11] = ["big", "small", "small",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 	viewStates[12] = ["big", "small", "small",
 					 "medium", "big",
-					 "small", "small", "big"];
+					 "small", "small", "big",
+           "medium", "big"];
 
 
-     
-    var factory = {}; 
- 
+
+    var factory = {};
+
     factory.getViewStates = function() {
             return viewStates;
         }
- 
+
     return factory;
 });
 
 app.controller('FeaturesController', ['$scope','FeauresService' , function($scope, FeauresService) {
 
   	this.isActive = new Array(12);
-  	this.isSelected = new Array(12);  	
+  	this.isSelected = new Array(12);
   	this.allStates = FeauresService.getViewStates();
   	this.currentState = this.allStates[0].slice(); //default state
 
-  	this.ToggleSelect = function(index){  		
-  		this.isSelected[index] = !this.isSelected[index]; 
-  		this.deselectOthers(index);	
+  	this.ToggleSelect = function(index){
+  		this.isSelected[index] = !this.isSelected[index];
+  		this.deselectOthers(index);
 
   		if(index === 0 || index === 1 || index === 2){
 
   			this.currentState[0] =  this.allStates[index][0];
   			this.currentState[1] =  this.allStates[index][1];
-  			this.currentState[2] =  this.allStates[index][2];	
+  			this.currentState[2] =  this.allStates[index][2];
 
   		} else if (index === 3 || index === 4 ){
 
   			this.currentState[3] =  this.allStates[index][3];
-  			this.currentState[4] =  this.allStates[index][4];	
+  			this.currentState[4] =  this.allStates[index][4];
 
   		} else if (index === 5 || index === 6 || index === 7){
 
   			this.currentState[5] =  this.allStates[index][5];
   			this.currentState[6] =  this.allStates[index][6];
-  			this.currentState[7] =  this.allStates[index][7];	
+  			this.currentState[7] =  this.allStates[index][7];
 
-  		}  		
-  		
+  		} else if (index === 8 || index === 9 ){
+
+        this.currentState[8] =  this.allStates[index][8];
+        this.currentState[9] =  this.allStates[index][9];
+
+      }
+
   	};
 
   	this.deselectAll = function(){
@@ -97,7 +115,7 @@ app.controller('FeaturesController', ['$scope','FeauresService' , function($scop
   	};
 
   	this.deselectOthers = function(index){
-  		for (var i = 0; i < this.isSelected.length; ++i) { 
+  		for (var i = 0; i < this.isSelected.length; ++i) {
   			if(index !== i) this.isSelected[i] = false; }
   	};
 
@@ -106,14 +124,14 @@ app.controller('FeaturesController', ['$scope','FeauresService' , function($scop
   	};
 
   	this.dezactivateOthers = function(index){
-  		for (var i = 0; i < this.isActivated.length; ++i) { 
+  		for (var i = 0; i < this.isActivated.length; ++i) {
   			if(index !== i) this.isActivated[i] = false;
   		}
   	};
-	
+
 }]);
 
-app.config(function($stateProvider, $urlRouterProvider, $stickyStateProvider){ 
+app.config(function($stateProvider, $urlRouterProvider, $stickyStateProvider){
 
 	$urlRouterProvider.otherwise("/")
 
@@ -128,8 +146,8 @@ app.config(function($stateProvider, $urlRouterProvider, $stickyStateProvider){
 	.state('root.products', {
 		url: "products",
 		views: {
-			"products-view@": { 
-				templateUrl: "products-panels"				
+			"products-view@": {
+				templateUrl: "products-panels"
 			}
 		},
 		onEnter: function(){
